2c545826bb11a5b014758fe6249dd2532949d345
aneurysm <aneurysmjs@outlook.com>
2023-05-05 19:06:25 +0200

    feat(components): add BaseLayout
    use <BaseLayout /> on <App />

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <main className="base-layout">
+    <div className="base-layout__content">
+      <RouterView />
+    </div>
+    <TheFooter />
+  </main>
+</template>
+
+<style>
+.base-layout {
+  @apply flex flex-col h-screen;
+}
+
+.base-layout__content {
+  @apply grow mt-16;
+}
+</style>
