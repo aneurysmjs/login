899a2c317b777ff13ffed8229c579ff254aba339
aneurysmjs <aneurysmjs@outlook.com>
2023-06-25 12:45:01 +0200

    feat: setup tanstackquery

---
+++ src/composables/useUserQueries.ts
@@ -0,0 +1,114 @@
+import { type Ref, ref } from 'vue'
+import { useQuery } from '@tanstack/vue-query'
+
+export interface Person {
+  firstName: string
+  lastName: string
+  age: number
+  visits: number
+  status: string
+  progress: number
+}
+
+const defaultData: Person[] = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    visits: 10,
+    status: 'Active',
+    progress: 0.8,
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Smith',
+    age: 32,
+    visits: 15,
+    status: 'Inactive',
+    progress: 0.5,
+  },
+  {
+    firstName: 'Michael',
+    lastName: 'Johnson',
+    age: 41,
+    visits: 5,
+    status: 'Active',
+    progress: 0.9,
+  },
+  {
+    firstName: 'Emily',
+    lastName: 'Brown',
+    age: 29,
+    visits: 20,
+    status: 'Inactive',
+    progress: 0.3,
+  },
+  {
+    firstName: 'David',
+    lastName: 'Wilson',
+    age: 37,
+    visits: 8,
+    status: 'Active',
+    progress: 0.6,
+  },
+  {
+    firstName: 'Sarah',
+    lastName: 'Taylor',
+    age: 44,
+    visits: 12,
+    status: 'Inactive',
+    progress: 0.7,
+  },
+  {
+    firstName: 'Daniel',
+    lastName: 'Anderson',
+    age: 36,
+    visits: 18,
+    status: 'Active',
+    progress: 0.4,
+  },
+  {
+    firstName: 'Olivia',
+    lastName: 'Martinez',
+    age: 31,
+    visits: 7,
+    status: 'Inactive',
+    progress: 0.2,
+  },
+  {
+    firstName: 'Matthew',
+    lastName: 'Hernandez',
+    age: 27,
+    visits: 13,
+    status: 'Active',
+    progress: 0.8,
+  },
+  {
+    firstName: 'Sophia',
+    lastName: 'Garcia',
+    age: 39,
+    visits: 9,
+    status: 'Inactive',
+    progress: 0.5,
+  },
+]
+
+export default function useUserQueries() {
+  function getUsers(enabled: Ref<boolean> = ref(true)) {
+    return useQuery({
+      queryKey: ['users'],
+      enabled,
+      queryFn: (): Promise<Person[]> => new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(defaultData)
+        }, 1500)
+      }),
+      //  select: res => res.data,
+      refetchOnWindowFocus: false,
+    })
+  }
+
+  return {
+    getUsers,
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { VueQueryPlugin, type VueQueryPluginOptions } from '@tanstack/vue-query'
 import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
@@ -7,10 +8,22 @@ import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
 
+const vueQueryPluginOptions: VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+      },
+    },
+  },
+}
+
 const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 app.use(router)
+app.use(VueQueryPlugin, vueQueryPluginOptions)
 app.mount('#app')
